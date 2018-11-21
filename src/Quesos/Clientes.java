6bd19285e3e58cd2f07b0b5367ebd1efd479e6fc
ejmarinc <ejmarinc@hotmail.com>
2018-11-21 00:34:10 -0600

    first commit

---
+++ src/Quesos/Clientes.java
@@ -0,0 +1,13 @@
+package Quesos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Clientes {
+    private String cedula;
+    private String nombre;
+    private HashMap<String, ArrayList> kilosCosto;
+
+}
